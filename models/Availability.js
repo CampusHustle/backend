8a0d42032692aba3a0851c084c6ebdf9afdafa36
Efrata Endalkachew Yehun <efrataendalkachew05@gmail.com>
2026-08-15 19:17:10 +0300

    fixed availablity.js

--- models/Availability.js
+++ models/Availability.js
@@ -1,39 +1,60 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const availabilitySchema = new mongoose.Schema(
   {
     tutorId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: [true, 'Tutor ID is required'],
-      index: true
+      ref: "User",
+      required: true,
+      index: true,
     },
+
     dayOfWeek: {
       type: String,
-      enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-      required: [true, 'Day of week is required']
+      enum: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      required: true,
     },
+
     startTime: {
       type: String,
-      required: [true, 'Start time is required'],
-      match: [/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, 'Start time must be in HH:MM format']
+      required: true,
+      match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/,
     },
+
     endTime: {
       type: String,
-      required: [true, 'End time is required'],
-      match: [/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, 'End time must be in HH:MM format']
+      required: true,
+      match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/,
     },
+
     isBooked: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
+
+    imgUrl: {
+      type: String,
+      required: true,
+      default: "https://placehold.co/600x400?text=Image+Not+Found",
+    },
   },
   {
-    timestamps: true
-  }
+    timestamps: true,
+  },
 );
 
-// Compound index to quickly fetch open slots for a tutor
-availabilitySchema.index({ tutorId: 1, dayOfWeek: 1, isBooked: 1 });
+availabilitySchema.index({
+  tutorId: 1,
+  dayOfWeek: 1,
+  isBooked: 1,
+});
 
-export const Availability = mongoose.model('Availability', availabilitySchema);
+export const Availability = mongoose.model("Availability", availabilitySchema);
